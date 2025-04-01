f8dd167e825444e2e08ff5e2239d9e6d46929a09
AlexOne007 <alex.am.chaos@gmail.com>
2025-04-01 22:11:46 +0300

    multimod

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from GradientDescent import GradientDescent
 from LearningRateScheduling import LearningRateSchedulingConstant, LearningRateSchedulingLinear, \
     LearningRateSchedulingExponential, LearningRateSchedulingPolynomial
@@ -8,6 +10,7 @@ func_table = [
     [lambda x: x[0] ** 2 - 10, lambda x: 2 * x[0]],
     [lambda x: x[0] ** 2 + x[1] ** 2, lambda x: [2 * x[0], 2 * x[1]]],
     [lambda x: x[0] ** 2 + 20 * x[1] ** 2 - 8 * x[0] * x[1] - x[1], lambda x: [2 * x[0] - 8 * x[1], 40 * x[1] - 8 * x[0] - 1]],
+    [lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2, lambda x: [2 * (x[0] ** 2 + x[1] - 11) * (2 * x[0]) + 2 * (x[0] + x[1] ** 2 - 7), 2 * (x[0] + x[1] ** 2 - 7) * (2*x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]]
 ]
 
 
@@ -28,8 +31,8 @@ if __name__ == "__main__":
     # run(2, Armijo(10, 0.9, 0.00001, 0.001), SequenceValueEps(0.0001), [2, 0], 0)
     # print("--------")
     # run(2, Wolfe(1,0.99, 0.00001, 0.001, 0.4), SequenceValueEps(10 ** -10), [2, 0], 0)
-    run(2, Armijo(30, 0.99, 0.0001, 0.001), SequenceValueEps(10 ** -12), [10, -12], 5 * 10 ** 4)
-    run(2, Wolfe(30, 0.0001, 0.9, 0.001), SequenceValueEps(10 ** -12), [10, -12], 5 * 10 ** 4)
+    run(3, Armijo(1, 0.9, 0.0001, 0.001), SequenceEps(10 ** -10), [np.longfloat(3), np.longfloat(3)], 5 * 10 ** 3)
+    run(3, Wolfe(12, 0.001, 0.1, 0.0001), SequenceEps(10 ** -10), [3, 3], 5 * 10 ** 3)
     # run(1, Armijo(1, 0.99, 0.00001, 0.001), SequenceEps(10 ** -10), [10], 5 * 10 ** 6)
     # run(1, Wolfe(1,0.99, 0.00001, 0.001, 0.4), SequenceEps(10 ** -10), [10], 5 * 10 ** 6)
     # print("--------")
