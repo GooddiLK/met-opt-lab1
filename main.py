2e1d08454af29bb4c45fcacc4e5d5302e1912806
alekseyplastinin <alexey.plastinin2016@yandex.ru>
2025-04-03 11:12:44 +0300

    func_table fix

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from StoppingCriteria import Iterations, SequenceEps, SequenceValueEps
 
 func_table = [
     [lambda x: x[0] ** 2 - 10, lambda x: 2 * x[0]],
+
     [lambda x: x[0] ** 2 + x[1] ** 2, lambda x: [2 * x[0], 2 * x[1]]],
+
     [lambda x: x[0] ** 2 + 20 * x[1] ** 2 - 8 * x[0] * x[1] - x[1],
      lambda x: [2 * x[0] - 8 * x[1], 40 * x[1] - 8 * x[0] - 1]],
+
     [lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2,
      lambda x: [2 * (x[0] ** 2 + x[1] - 11) * (2 * x[0]) + 2 * (x[0] + x[1] ** 2 - 7),
-                2 * (x[0] + x[1] ** 2 - 7) * (2 * x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]]
+                2 * (x[0] + x[1] ** 2 - 7) * (2 * x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]],
 ]
 
 
@@ -33,12 +36,9 @@ def run(func_number, learning_rate, stopping_criteria, point, iterations):
 
 if __name__ == "__main__":
     # run(2, LearningRateSchedulingPolynomial(0.5, 1), SequenceValueEps(0.0001), [2, 0], 0)
-    # print("--------")
     # run(2, Armijo(10, 0.9, 0.00001, 0.001), SequenceValueEps(0.0001), [2, 0], 0)
-    # print("--------")
     # run(2, Wolfe(1,0.99, 0.00001, 0.001, 0.4), SequenceValueEps(10 ** -10), [2, 0], 0)
     run(3, Armijo(1, 0.9, 0.0001, 0.001), SequenceEps(10 ** -10), [np.longdouble(3), np.longdouble(3)], 5 * 10 ** 3)
     run(3, Wolfe(12, 0.001, 0.1, 0.0001), SequenceEps(10 ** -10), [3, 3], 5 * 10 ** 3)
     # run(1, Armijo(1, 0.99, 0.00001, 0.001), SequenceEps(10 ** -10), [10], 5 * 10 ** 6)
     # run(1, Wolfe(1,0.99, 0.00001, 0.001, 0.4), SequenceEps(10 ** -10), [10], 5 * 10 ** 6)
-    # print("--------")
